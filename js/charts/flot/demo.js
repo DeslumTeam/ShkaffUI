4bdb3495a0c24bb036b464ecaa6b0167921f4dd9
deslum <yurij-bukatkin@yandex.ru>
2018-05-15 01:21:52 +0300

    Add Chart

--- js/charts/flot/demo.js
+++ js/charts/flot/demo.js
@@ -9,6 +9,7 @@ $(function(){
       data: Math.floor(Math.random() * 100) + 1
     }
   }
+  var colors = ["#039808","#EAF208","#EB1111"]
   $("#flot-pie-operator").length && $.plot($("#flot-pie-operator"), da1, {
     series: {
       pie: {
@@ -16,7 +17,7 @@ $(function(){
         show: true
       }
     },
-    colors: ["#99c7ce","#999999","#bbbbbb"],
+    colors: colors,
     grid: {
         hoverable: true,
         clickable: false
@@ -27,38 +28,38 @@ $(function(){
     }
   });
 
-//   $("#flot-pie-dump").length && $.plot($("#flot-pie-dump"), da1, {
-//     series: {
-//       pie: {
-//         innerRadius: 0.5,
-//         show: true
-//       }
-//     },
-//     colors: ["#99c7ce","#999999","#bbbbbb","#dddddd","#f0f0f0"],
-//     grid: {
-//         hoverable: true,
-//         clickable: false
-//     },
-//     tooltip: true,
-//     tooltipOpts: {
-//       content: "%s: %p.0%"
-//     }
-//   });
-//   $("#flot-pie-restore").length && $.plot($("#flot-pie-restore"), da1, {
-//     series: {
-//       pie: {
-//         innerRadius: 0.5,
-//         show: true
-//       }
-//     },
-//     colors: ["#99c7ce","#999999","#bbbbbb","#dddddd","#f0f0f0"],
-//     grid: {
-//         hoverable: true,
-//         clickable: false
-//     },
-//     tooltip: true,
-//     tooltipOpts: {
-//       content: "%s: %p.0%"
-//     }
-//   });
+  $("#flot-pie-dump").length && $.plot($("#flot-pie-dump"), da1, {
+    series: {
+      pie: {
+        innerRadius: 0.5,
+        show: true
+      }
+    },
+    colors: colors,
+    grid: {
+        hoverable: true,
+        clickable: false
+    },
+    tooltip: true,
+    tooltipOpts: {
+      content: "%s: %p.0%"
+    }
+  });
+  $("#flot-pie-restore").length && $.plot($("#flot-pie-restore"), da1, {
+    series: {
+      pie: {
+        innerRadius: 0.5,
+        show: true
+      }
+    },
+    colors: colors,
+    grid: {
+        hoverable: true,
+        clickable: false
+    },
+    tooltip: true,
+    tooltipOpts: {
+      content: "%s: %p.0%"
+    }
+  });
 });
